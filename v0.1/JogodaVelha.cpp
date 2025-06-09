f251246d25086fc7225f58a52de8362ade3a28b8
Arthur do Nascimento Rodrigues <atrodrigues2015@gmail.com>
2025-06-09 20:08:11 -0300

    Trabalho

--- v0.1/JogodaVelha.cpp
+++ v0.1/JogodaVelha.cpp
@@ -5,24 +5,24 @@ using namespace std;
 char n1 = '-', n2 = '-', n3 = '-', n4 = '-', n5 = '-', n6 = '-', n7 = '-', n8 = '-', n9 = '-'; // Inicialização do tabuleiro
 char p1, p2;// Simbolo do jogador 1 e jogador 2.
 
-void tabuleiro(int coluna, int linha, char simbolo){
-    if (coluna == 0 && linha == 0){
+void tabuleiro(int linha, int coluna, char simbolo){
+    if (linha == 0 && coluna == 0){
         n1 = simbolo;
-    }else if (coluna == 0 && linha == 1){
+    }else if (linha == 0 && coluna == 1){
         n2 = simbolo;
-    }else if (coluna == 0 && linha == 2){
+    }else if (linha == 0 && coluna == 2){
         n3 = simbolo;
-    }else if (coluna == 1 && linha == 0){
+    }else if (linha == 1 && coluna == 0){
         n4 = simbolo;
-    }else if (coluna == 1 && linha == 1){
+    }else if (linha == 1 && coluna == 1){
         n5 = simbolo;
-    }else if (coluna == 1 && linha == 2){
+    }else if (linha == 1 && coluna == 2){
         n6 = simbolo;
-    }else if (coluna == 2 && linha == 0){
+    }else if (linha == 2 && coluna == 0){
         n7 = simbolo;
-    }else if (coluna == 2 && linha == 1){
+    }else if (linha == 2 && coluna == 1){
         n8 = simbolo;
-    }else if (coluna == 2 && linha == 2){
+    }else if (linha == 2 && coluna == 2){
         n9 = simbolo;
     }
 
@@ -52,13 +52,12 @@ int main(){
         cout << "Simbolo ja escolhido! Player 2 digite novamente seu simbolo: ";
         cin >> p2;
     }
-
-    while (p1 == '-' || p2 == '-'){ //Verificação para não pode escolher '-'.
-        if (p1 == '-'){
+    while ((p1 == '-' || p2 == '-') || (p1 == p2)){ //Verificação para não pode escolher '-' e p1 = p2.
+        if (p1 == '-' || p1 == p2){
             cout << "Player 1 simbolo invalido! Digite outro simbolo: ";
             cin >> p1;
         }
-        if (p2 == '-'){
+        if (p2 == '-' || p2 == p1){
             cout << "Player 2 simbolo invalido! Digite outro simbolo: ";
             cin >> p2;
         }
@@ -75,20 +74,20 @@ int main(){
         if (i % 2 != 0){ // Numero impar, player 1 joga.
         cout << endl; // Espaço
         cout << "Player 1!" << endl;
-        cout << "Digite a coluna(0-2): ";
-        cin >> coluna;
+        cout << "Digite a linha(0-2): ";
+        cin >> linha;
         
-            if (coluna > 2 || coluna < 0){
-                cout << "Valor de coluna invalido! Digite novamente a coluna: ";
-                cin >> coluna;
-            }
-            cout << "Digite a linha(0-2): ";
-            cin >> linha;
-            if (linha > 2 || linha < 0){
+            while(linha > 2 || linha < 0){
                 cout << "Valor de linha invalido! Digite novamente a linha: ";
                 cin >> linha;
             }
-            tabuleiro(coluna, linha, p1);
+        cout << "Digite a coluna(0-2): ";
+        cin >> coluna;
+            while(coluna > 2 || coluna < 0){
+                cout << "Valor de coluna invalido! Digite novamente a coluna: ";
+                cin >> coluna;
+            }
+            tabuleiro(linha, coluna, p1);
 
             if ((n1 == p1 && n2 == p1 && n3 == p1) || // Verifica se o player 1 vençeu
                 (n4 == p1 && n5 == p1 && n6 == p1) ||
@@ -104,21 +103,19 @@ int main(){
         }else{ //Numero par, Player 2 joga.
         cout << endl; // Espaço
         cout << "Player 2!" << endl;
-        cout << "Digite a coluna(0-2): ";
-        cin >> coluna;
-            if (coluna > 2 || coluna < 0){
-                cout << "Valor de coluna invalido!" << endl
-                     << "Digite a coluna: ";
-                cin >> coluna;
-            }
-            cout << "Digite a linha(0-2): ";
-            cin >> linha;
-            if (linha > 2 || linha < 0){
-                cout << "Valor invalido" << endl
-                     << "Digite a linha: ";
+        cout << "Digite a linha(0-2): ";
+        cin >> linha;
+            while(linha > 2 || linha < 0){
+                cout << "Valor de linha invalido! Digite novamente a linha: ";
                 cin >> linha;
             }
-            tabuleiro(coluna, linha, p2);
+        cout << "Digite a coluna(0-2): ";
+        cin >> coluna;
+            while(coluna > 2 || coluna < 0){
+                cout << "Valor de coluna invalido! Digite novamente a coluna: ";
+                cin >> coluna;
+            }
+            tabuleiro(linha, coluna, p2);
 
             if ((n1 == p2 && n2 == p2 && n3 == p2) || // Verifica se o player 2 vençeu
                 (n4 == p2 && n5 == p2 && n6 == p2) ||
